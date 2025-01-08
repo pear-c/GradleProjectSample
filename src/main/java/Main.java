8ccfc17f7fadba97e14e20f3dbd0e79f05d52ab1
pear_c <zxcv9676@naver.com>
2025-01-08 09:11:02 +0900

    컬렉션 프레임워크, Stream API

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import com.google.common.collect.ImmutableList;
+
+public class Main {
+    public static void main(String[] args) {
+        ImmutableList<String> fruits = ImmutableList.of("Apple", "Banana", "Orange");
+        System.out.println("Fruits: " + fruits);
+
+    }
+}
