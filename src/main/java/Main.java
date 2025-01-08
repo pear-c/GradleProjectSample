9f3a90aab8866d9d97073c62d8548c9c65f6f91f
pear_c <zxcv9676@naver.com>
2025-01-08 09:17:36 +0900

    실습 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
-import com.google.common.collect.ImmutableList;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        ImmutableList<String> fruits = ImmutableList.of("Apple", "Banana", "Orange");
-        System.out.println("Fruits: " + fruits);
+    public static void main(String[] args) throws Exception{
+        String[] fruitsArray = {"Banana","Mango","Graph","Orange","Apple"};
+        List<String> fruitsList = Arrays.asList("Banana","Mango","Graph","Orange","Apple");
+
+        System.out.println("배열: ");
+        for(String fruit : fruitsArray) {
+            System.out.println(fruit + " ");
+        }
+        System.out.println("ArrayList: " + fruitsList);
 
     }
 }
